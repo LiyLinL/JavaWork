8441717978bfc6a7f7d676f72b7ca5bd90f8a284
LiyLinL <vip860180@gmail.com>
2020-04-20 14:59:29 +0800

    enum

---
+++ enum/src/com/run/liy/mainTest.java
@@ -0,0 +1,36 @@
+package com.run.liy;
+
+public class mainTest {
+
+   public static void main(String[] args) {
+//      String[] sfcList = {"11111", "22222", "33333", "44444", "55555"};
+//
+//      Map<String, String> map = new HashMap<>();
+//      map.put("11111", "A");
+//      map.put("22222", "B");
+//      map.put("33333", "A");
+//      map.put("44444", "C");
+//      map.put("55555", "A");
+//
+//
+//      Map<String, List<String>> mapObj = new HashMap<>();
+//
+//      for ( String s : sfcList ) {
+//         if (mapObj.get(map.get(s)) == null ) {
+//            List<String> lists = new ArrayList<>();
+//            lists.add(s);
+//            mapObj.put(map.get(s), lists);
+//         }
+//         else {
+//            mapObj.get(map.get(s)).add(s);
+//         }
+//      }
+//
+//      Iterator<Map.Entry<String, List<String>>> m = mapObj.entrySet().iterator();
+//      while (m.hasNext()) {
+//            Map.Entry<String, List<String>> entry = m.next();
+//            System.out.println(entry.getKey() + "==" + entry.getValue());
+//      }
+
+   }
+}
