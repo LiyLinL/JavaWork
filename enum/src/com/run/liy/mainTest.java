d679e84360687299d6498f0d5ad1eb60c77cd674
LiyLinL <vip860180@gmail.com>
2020-05-07 16:37:59 +0800

    enum

--- enum/src/com/run/liy/mainTest.java
+++ enum/src/com/run/liy/mainTest.java
@@ -1,8 +1,11 @@
 package com.run.liy;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class mainTest {
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 //      String[] sfcList = {"11111", "22222", "33333", "44444", "55555"};
 //
 //      Map<String, String> map = new HashMap<>();
@@ -31,6 +34,32 @@ public class mainTest {
 //            Map.Entry<String, List<String>> entry = m.next();
 //            System.out.println(entry.getKey() + "==" + entry.getValue());
 //      }
+        Calendar cal = Calendar.getInstance();
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+        int w = cal.get(Calendar.WEEK_OF_YEAR);
+        System.out.println(w);
+        if (w % 2 != 0) {
+            Date one = getThisWeekMonday(cal.getTime());
+            cal.setTime(one);
+            cal.add(Calendar.DATE, -7);
+            System.out.println(cal.getTime());
+        }
+    }
 
-   }
+    public static Date getThisWeekMonday(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        // 获得当前日期是一个星期的第几天
+        int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+        if (1 == dayWeek) {
+            cal.add(Calendar.DAY_OF_MONTH, -1);
+        }
+        // 设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+        // 获得当前日期是一个星期的第几天
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        // 根据日历的规则，给当前日期减去星期几与一个星期第一天的差值
+        cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
+        return cal.getTime();
+    }
 }
