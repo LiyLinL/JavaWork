28595b6af46db590a4696fbfb86cfad3fb1658cf
LiyLinL <vip860180@gmail.com>
2020-05-11 14:17:21 +0800

    enum

--- enum/src/com/run/liy/main.java
+++ enum/src/com/run/liy/main.java
@@ -1,73 +1,67 @@
 package com.run.liy;
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import java.util.Properties;
+import java.text.ParseException;
 
 public class main {
-    private static String to = "vip860180@gmail.com";
-
-    private static String from = "vip860180@gmail.com";
-
-    private static String usr = "vip860180";
-
-    private static String pas = "0938860180";
-
-    private static String host = "smtp.gmail.com";
+//    private static String to = "vip860180@gmail.com";
+//
+//    private static String from = "vip860180@gmail.com";
+//
+//    private static String usr = "vip860180";
+//
+//    private static String pas = "0938860180";
+//
+//    private static String host = "smtp.gmail.com";
     // for test something
-    public static void main(String[] args) {
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.transport.protocol", "smtp");
-        properties.setProperty("mail.smtp.host", host);
-        properties.setProperty("mail.smtp.port", "465");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.ssl.enable", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-
-        Session session = Session.getInstance(properties, new Authenticator() {
-            protected PasswordAuthentication passwordAuthentication() {
-                return new PasswordAuthentication(usr, pas);
-            }
-        });
-
-        String subject = "主旨";
-        String body = "內文";
-
-        try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-            message.setSubject(subject);
-
-            Multipart multipart = new MimeMultipart();
-            BodyPart messageBodyPart = new MimeBodyPart();
-            messageBodyPart.setText(body);
-            multipart.addBodyPart(messageBodyPart);
-
-            /*
-            BodyPart fileBodyPart = new MimeBodyPart(); //宣告一個BodyPart用以夾帶附加檔案
-            String filename = "要送的檔案路徑";  //要夾帶的檔案名稱
-            DataSource source = new FileDataSource(filename);//讀取檔案
-            fileBodyPart.setDataHandler(new DataHandler(source));
-            fileBodyPart.setFileName("要顯示的檔案名稱"); //設定附加檔案顯示的名稱
-            multipart.addBodyPart(fileBodyPart);//把BodyPart加入Multipart（這個part夾帶檔案）
-			*/
-            message.setContent(multipart); //設定eMultipart為messag的Content
-            Transport transport = session.getTransport("smtp");
-            transport.connect(host, usr, pas);
-            //傳送信件
-            transport.sendMessage(message, message.getAllRecipients());
-
-            System.out.println("發送成功");
-            //關閉連線
-            transport.close();
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-        }
-
+    public static void main(String[] args) throws ParseException {
+//        Properties properties = System.getProperties();
+//        properties.setProperty("mail.transport.protocol", "smtp");
+//        properties.setProperty("mail.smtp.host", host);
+//        properties.setProperty("mail.smtp.port", "465");
+//        properties.put("mail.smtp.auth", "true");
+//        properties.put("mail.smtp.ssl.enable", "true");
+//        properties.put("mail.smtp.starttls.enable", "true");
+//
+//        Session session = Session.getInstance(properties, new Authenticator() {
+//            protected PasswordAuthentication passwordAuthentication() {
+//                return new PasswordAuthentication(usr, pas);
+//            }
+//        });
+//
+//        String subject = "主旨";
+//        String body = "內文";
+//
+//        try {
+//            MimeMessage message = new MimeMessage(session);
+//            message.setFrom(new InternetAddress(from));
+//            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+//
+//            message.setSubject(subject);
+//
+//            Multipart multipart = new MimeMultipart();
+//            BodyPart messageBodyPart = new MimeBodyPart();
+//            messageBodyPart.setText(body);
+//            multipart.addBodyPart(messageBodyPart);
+//
+//            /*
+//            BodyPart fileBodyPart = new MimeBodyPart(); //宣告一個BodyPart用以夾帶附加檔案
+//            String filename = "要送的檔案路徑";  //要夾帶的檔案名稱
+//            DataSource source = new FileDataSource(filename);//讀取檔案
+//            fileBodyPart.setDataHandler(new DataHandler(source));
+//            fileBodyPart.setFileName("要顯示的檔案名稱"); //設定附加檔案顯示的名稱
+//            multipart.addBodyPart(fileBodyPart);//把BodyPart加入Multipart（這個part夾帶檔案）
+//			*/
+//            message.setContent(multipart); //設定eMultipart為messag的Content
+//            Transport transport = session.getTransport("smtp");
+//            transport.connect(host, usr, pas);
+//            //傳送信件
+//            transport.sendMessage(message, message.getAllRecipients());
+//
+//            System.out.println("發送成功");
+//            //關閉連線
+//            transport.close();
+//        } catch (MessagingException mex) {
+//            mex.printStackTrace();
+//        }
         }
 }
--- enum/src/com/run/liy/mainTest.java
+++ enum/src/com/run/liy/mainTest.java
@@ -37,13 +37,14 @@ public class mainTest {
         Calendar cal = Calendar.getInstance();
         cal.setFirstDayOfWeek(Calendar.MONDAY);
         int w = cal.get(Calendar.WEEK_OF_YEAR);
-        System.out.println(w);
+//        System.out.println(w);
         if (w % 2 != 0) {
             Date one = getThisWeekMonday(cal.getTime());
             cal.setTime(one);
             cal.add(Calendar.DATE, -7);
-            System.out.println(cal.getTime());
+//            System.out.println(cal.getTime());
         }
+        Test();
     }
 
     public static Date getThisWeekMonday(Date date) {
@@ -57,5 +58,75 @@ public class mainTest {
         int day = cal.get(Calendar.DAY_OF_WEEK);
         cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
         return cal.getTime();
+    }
+
+    public static void Test() {
+        String a = "AXA53450001ABCDA";
+        char[] c = a.toCharArray();
+        int k = 0;
+        for (int i = 1; c.length > i; i += 2) {
+            k += rt(c[i]);
+        }
+        System.out.println(k*3);
+        int t = 0;
+        for (int i = 0; c.length > i; i += 2) {
+            t += rt(c[i]);
+        }
+        System.out.println(t);
+    }
+
+    public static int rt(char a) {
+        switch (a) {
+            case 'A':
+                return 10;
+            case 'B':
+                return 11;
+            case 'C':
+                return 12;
+            case 'D':
+                return 13;
+            case 'E':
+                return 14;
+            case 'F':
+                return 15;
+            case 'G':
+                return 16;
+            case 'H':
+                return 17;
+            case 'J':
+                return 18;
+            case 'K':
+                return 19;
+            case 'L':
+                return 20;
+            case 'M':
+                return 21;
+            case 'N':
+                return 22;
+            case 'P':
+                return 23;
+            case 'Q':
+                return 24;
+            case 'R':
+                return 25;
+            case 'S':
+                return 26;
+            case 'T':
+                return 27;
+            case 'U':
+                return 28;
+            case 'V':
+                return 29;
+            case 'W':
+                return 30;
+            case 'X':
+                return 31;
+            case 'Y':
+                return 32;
+            case 'Z':
+                return 33;
+            default:
+                return Integer.parseInt(String.valueOf(a));
+        }
     }
 }
