df1fb3a7d36bcde2a96858699acc4c618aa25fab
LiyLinL <vip860180@gmail.com>
2020-07-07 14:14:56 +0800

    com

--- generator/src/test/java/com/liy/generator/GeneratorApplicationTests.java
+++ generator/src/test/java/com/liy/generator/GeneratorApplicationTests.java
@@ -215,7 +215,6 @@ public class GeneratorApplicationTests {
         jackson.setSome("AAA");
 
         String json = objectMapper.writeValueAsString(jackson);
-        Map<String, String> map = objectMapper.readValue(json, new TypeReference<Map<String, String>>() {
-        });
+        Map<String, String> map = objectMapper.readValue(json, new TypeReference<Map<String, String>>() {});
     }
 }
