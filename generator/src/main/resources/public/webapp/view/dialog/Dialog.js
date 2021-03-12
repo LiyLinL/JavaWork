cd1364aa25a7e699872c948dfa53fe14e947b1cd
LiyLinL <vip860180@gmail.com>
2021-03-12 13:50:26 +0800

    commit

--- generator/src/main/resources/public/webapp/view/dialog/Dialog.js
+++ generator/src/main/resources/public/webapp/view/dialog/Dialog.js
@@ -1,18 +1,16 @@
 sap.ui.define([
    'sap/ui/core/Fragment',
-   "sap/ui/base/Object",
-   'sap/ui/model/json/JSONModel',
-   'sap/m/MessageBox'
-], function (Fragment, Object, JSONModel, MessageBox) {
+   "sap/ui/base/Object"
+], function (Fragment, Object) {
    "use strict";
-   return Object.extend("gen.view.single.Dialog", {
+   return Object.extend("gen.view.dialog.Dialog", {
       showDialog: function (me) {
          this.me = me;
 
          if (!this.dialog) {
             this.dialog = Fragment.load({
                id: 'Dialog',
-               name: "gen.view.single.Dialog",
+               name: "gen.view.dialog.Dialog",
                controller: this
             }).then(function (oDialog) {
                oDialog.setTitle('T');
---
+++ generator/src/main/resources/public/webapp/view/lib/cTable.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+      'sap/ui/table/Table'
+   ],
+   function (Table) {
+      "use strict";
+      var t = Table.extend('gen.view.lib.cTable', {
+         metadata: {
+            library: 'gen.view.lib'
+         },
+         invalidate: function () {
+            Table.prototype.invalidate.call(this);
+         },
+         renderer: function (oRm, oTable) {
+            sap.ui.table.TableRenderer.render(oRm, oTable);
+            oRm.write("<Label>");
+            oRm.write(":");
+            oRm.write("</Label>");
+         },
+         onAfterRendering: function () {
+            Table.prototype.onAfterRendering.apply(this);
+         }
+      });
+      return t;
+   });
